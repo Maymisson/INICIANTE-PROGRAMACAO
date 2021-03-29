7da5596bc4368a958208061936fa1f879b83c9cd
maymisson <maymisson@gmail.com>
2021-03-29 18:51:11 -0300

    LP II  - Conclusão : Pong feito no P5.js

--- Lógica de Programação II/Pong JS/sketch.js
+++ Lógica de Programação II/Pong JS/sketch.js
@@ -9,22 +9,39 @@ let speedxBall = 5;
 let speedyBall = 5;
 
 // Variaveis da Raquete
-let xRacket = 5;
+let xRacket = 1;
 let yRacket = 100;
 let lRacket = 10;
 let hRacket = 60;
 
 // Variaveis Oponente
-let xRacket2 = 385;
+let xRacket2 = 388;
 let yRacket2 = 110;
 let speedyRacket2;
 
 
 // Variaveis de colisão
 hit = false;
+let chanceError = 0;
+
+// Variaveis do Placar
+let myPoint = 0;
+let rivalPoint = 0;
+
+// Variaveis de Sons
+let soundRacket;
+let soundPoint; 
+let soundTrail;
+
+function preload() {
+  soundRacket = loadSound("./sounds/racket.mp3");
+  soundPoint = loadSound("./sounds/point.mp3");
+  soundTrail = loadSound("./sounds/trail.mp3");
+}
 
 function setup() {
   createCanvas(400, 300);
+  soundTrail.loop();
 }
 
 function draw() {
@@ -39,6 +56,8 @@ function draw() {
   //hitRacket();
   hitRacketLib(xRacket, yRacket);
   hitRacketLib(xRacket2, yRacket2);
+  score();
+  markPoint();
 }
 
 function showBall() {
@@ -74,7 +93,8 @@ function moveRacket() {
 
 /*function hitRacket() {
   if ((xBall - raio < xRacket + lRacket) && (yBall - raio       < yRacket + hRacket) && (yBall + raio > yRacket)) {
-    speedxBall *= -1;        
+    speedxBall *= -1;
+    soundRacket.play();
   }
 }*/
 
@@ -83,13 +103,39 @@ function hitRacketLib(x, y) {
   hit = collideRectCircle(x, y, lRacket,               hRacket, xBall, yBall, raio);
   if (hit) {
     speedxBall *= -1;
+    soundRacket.play();
   }
 }
 
 function moveRacket2() {
-  speedyRacket2 = yBall - yRacket2 - lRacket / 2 -30;
-  yRacket2 += speedyRacket2;  
+  speedyRacket2 = yBall - yRacket2 - lRacket / 2 -25;
+  yRacket2 += speedyRacket2 * 0.12;  
 }
+
+function score() {
+  textAlign(CENTER);
+  textSize(16);
+  fill(color(255, 140, 0));
+  rect(230, 10, 40, 20);
+  fill(255);
+  text(rivalPoint, 250, 26);
+  fill(color(255, 140, 0));
+  rect(130, 10, 40, 20);
+  fill(255);
+  text(myPoint, 150, 26);
+}
+
+function markPoint() {
+  if (xBall >= 395) {
+        myPoint += 1;
+        soundPoint.play();
+      }
+  if (xBall <= 5) {
+        rivalPoint += 1;
+        soundPoint.play();
+      }
+}
+
 
 
 
